8e4681f59b27df520208fff8862bfe68bbd10a35
francis omondi <francisomondi17@gmail.com>
2023-06-19 02:02:28 +0300

    Intro to car catalogue includint ui for seach and filter

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,14 @@
+
+
+import { SearchManufacturerProps } from '@/app/types'
+import React from 'react'
+
+const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
+    return (
+        <div>
+            SearchManufacturer
+        </div>
+    )
+}
+
+export default SearchManufacturer
