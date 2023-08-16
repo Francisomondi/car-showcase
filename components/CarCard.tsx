37f9d4cfa55128d35f8ffa44ee9621a9549778c4
francis omondi <francisomondi17@gmail.com>
2023-08-16 18:26:41 +0300

    fetching data from cars api-pass

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@ import {useState} from 'react'
 import  Image  from "next/image";
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CarDetails from './CarDetails';
 
 interface CarCardProps{
@@ -31,7 +31,7 @@ const carRent= calculateCarRent(city_mpg,year)
 
         <div className='relative w-full h-40 my-3
         object-contain'>
-            <Image src={'/hero.png'} alt='car model' fill priority className='object-contain' />
+            <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
         </div>
         <div className='relative flex w-full mt-2'>
             <div className='flex group-hover:invisible w-full justify-between text-gray'>
